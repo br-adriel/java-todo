c15932f183d049992d8d12a654f3920d96b1cc9f
Adriel <adriel.fsantos@outlook.com>
2022-04-14 23:01:42 -0300

    Implementa metodo removeById

---
+++ src/main/java/controller/TaskController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import model.Task;
+import util.ConnectionFactory;
+
+/**
+ *
+ * @author adriel
+ */
+public class TaskController {
+
+    public void save(Task task) {
+        
+    }
+    
+    public void update(Task task) {
+        
+    }
+    
+    public void removeById(int taskId) throws SQLException {
+        String sql = "DELETE FROM tasks WHERE id = ?";
+        
+        Connection conn = null;
+        PreparedStatement statement = null;
+        
+        try {
+            conn = ConnectionFactory.getConnection();
+            
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, taskId); // substitui ? da string sql pelo id
+            statement.execute();
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao executar a tarefa");
+        } finally {
+            ConnectionFactory.closeConnection(conn);
+        }
+        
+    }
+    
+    public List<Task> getAll(int idProject) {
+        return null;
+    }
+}
