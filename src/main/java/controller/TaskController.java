17889315bcbd2ff79c56980f6f196da70e3ca497
Adriel <adriel.fsantos@outlook.com>
2022-04-16 22:40:42 -0300

    Adiciona metodo update

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -49,6 +49,40 @@ public class TaskController {
     }
     
     public void update(Task task) {
+        String sql = "UPDATE tasks SET "
+                + "idProject = ?, "
+                + "name = ?, "
+                + "description = ?, "
+                + "completed = ?, "
+                + "notes = ?, "
+                + "deadline = ?,"
+                + "createdAt = ?, "
+                + "updatedAt = ? "
+                + "WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try {
+            connection = ConnectionFactory.getConnection();
+            
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setBoolean(4, task.isIsCompleted());
+            statement.setString(5, task.getNotes());
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            statement.setInt(9, task.getId());
+
+            statement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao salvar a tarefa " + e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
         
     }
     
