48ef5c36fa411eb7bb0e63c20eb7c4588a6a7a89
Adriel <adriel.fsantos@outlook.com>
2022-04-16 23:16:11 -0300

    Adiciona ProjectController

---
+++ src/main/java/controller/ProjectController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.util.List;
+import model.Project;
+
+/**
+ *
+ * @author adriel
+ */
+public class ProjectController {
+
+    public void save(Project project) {
+
+    }
+
+    public void update(Project project) {
+
+    }
+
+    public void removeById(int projectId) {
+    }
+
+    public List<Project> getAll() {
+        return null;
+    }
+}
