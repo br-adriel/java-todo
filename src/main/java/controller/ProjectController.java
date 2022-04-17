60ca00bd0810687a0dd2bb6631a1f0e1e9a5d29c
Adriel <adriel.fsantos@outlook.com>
2022-04-16 23:22:27 -0300

    Implementa metodo update

--- src/main/java/controller/ProjectController.java
+++ src/main/java/controller/ProjectController.java
@@ -41,7 +41,32 @@ public class ProjectController {
     }
 
     public void update(Project project) {
+        String sql = "UPDATE projects SET "
+                + "name = ?, "
+                + "description = ?, "
+                + "createdAt = ?, "
+                + "updatedAt = ? "
+                + "WHERE id = ?";
 
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.setInt(5, project.getId());
+
+            statement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao atualizar o projeto" + e.getMessage(), e);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
     }
 
     public void removeById(int projectId) {
