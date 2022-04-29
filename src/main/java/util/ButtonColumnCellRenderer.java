8442da961a64799e87a5ce6d525447b15c1dd054
Adriel <adriel.fsantos@outlook.com>
2022-04-28 22:59:15 -0300

    Adiciona renderizador dos botoes editar e apagar

---
+++ src/main/java/util/ButtonColumnCellRenderer.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.awt.Component;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author adriel
+ */
+public class ButtonColumnCellRenderer extends DefaultTableCellRenderer {
+
+    private String buttonType;
+
+    public String getButtonType() {
+        return buttonType;
+    }
+
+    public void setButtonType(String buttonType) {
+        this.buttonType = buttonType;
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        if (isSelected) {
+            label.setIcon(new ImageIcon(getClass().getResource("/" + buttonType + "Selected.png")));
+        } else {
+            label.setIcon(new ImageIcon(getClass().getResource("/" + buttonType + ".png")));
+        }
+        return label;
+    }
+}
